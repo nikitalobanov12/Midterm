bd111f48510c0a10efdf47d45126f9af5aacf648
Nikita Lobanov <nikitaalobanovv@gmail.com>
2024-10-24 19:44:11 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 const fs = require("node:fs/promises");
 const { EOL } = require("node:os");
 
+//helper function to convert coffeeType to the full name & verify if the coffeeType is valid
 const verifyType = (type) => {
     if(type == "mr") return "medium-roast";
     if(type == "dr") return "dark-roast";
     if(type == "b") return "blonde";
     else throw new Error(`${type} is not a valid coffee type.`);
 }
+
+//helper function convert string to array 
 const splitLines = (string) => string.split(EOL);
 
+//helper function for removeInventory
+//loops through the array and deletes <quantity> amount of lines that match with <name>
+const deleteLines = (coffeeArray, name, quantity) => {
+    let deleteCounter = 0;
+    for (let i = 0; i < coffeeArray.length; i++) {
+        if(deleteCounter > quantity) break;
+        if(name == coffeeArray[i]) {
+            coffeeArray.splice(i, 1);
+            i--;
+            deleteCounter++;
+        }
+    }
+    // if the quantity input was '*' make it output the word "all" instead of the number of items deleted.
+    if(quantity == "*") deleteCounter = "all";
+    //if 1 item was deleted, set it to output "coffee", else output "coffees"
+    const coffeeOrCoffees = (deleteCounter == 1 ? 'coffee' : 'coffees');
+    console.log(`${deleteCounter} ${name} ${coffeeOrCoffees} deleted`);
+    //return the array to write back to the file
+    return coffeeArray;
+}
 
 const getInventory = (coffeeType)=> {
     const name = verifyType(coffeeType);
@@ -16,7 +39,8 @@ const getInventory = (coffeeType)=> {
     .then((result) => splitLines(result))
     .then ((result)=>{
         let count = 0;
-        for (const line of result) if(line == name) count++;
+        //loops through each line of the array, if the line matches the name that is inputted, increment the counter for how many of that type is in the stock.txt file
+        for (const line of result) if (line == name) count++;
         return count
     })
 }
@@ -24,7 +48,9 @@ const getInventory = (coffeeType)=> {
 const addInventory = (coffeeType) => {
     const name = verifyType(coffeeType);
     return fs.readFile("stock.txt", "utf-8")
+    // add the coffee type to the end of the string
     .then((result) => result += `${EOL}${name}`)
+    //write the modified string back to the stock.txt file
     .then((result) => fs.writeFile("stock.txt", result))
 }
 
@@ -32,21 +58,7 @@ const removeInventory = (coffeeType,  quantity) => {
     const name = verifyType(coffeeType);
     return fs.readFile('stock.txt', 'utf-8')
     .then((result) => splitLines(result))
-    .then((result) => {
-        let deleteCounter = 0;
-        for (let i = 0; i < result.length; i++) {
-            if(deleteCounter > quantity) break;
-            if(name == result[i]) {
-                result.splice(i, 1);
-                i--;
-                deleteCounter++;
-            }
-        }
-        if(quantity == "*") deleteCounter = "all";
-        const coffeeOrCoffees = (deleteCounter == 1 ? 'coffee' : 'coffees');
-        console.log(`${deleteCounter} ${name} ${coffeeOrCoffees} deleted`);
-        return result;
-    })
+    .then((result) => deleteLines(result, name, quantity))
     .then((result) => fs.writeFile('stock.txt', result.join(EOL)))
     .catch((err)=> console.error(`error: ${err}`))
 }
